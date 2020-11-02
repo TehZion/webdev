af1fd8c6d6e2b5c4ba101b0970f6ce17a3036920
teh yl <tehyongli97@gmail.com>
2020-11-02 12:32:54 +0800

    button update and table removed

--- yltablebooking/scripts/list.js
+++ yltablebooking/scripts/list.js
@@ -27,7 +27,7 @@ function GetBookings() {
     row.insertCell(2).innerHTML = email;
     row.insertCell(3).innerHTML = pax;
     row.insertCell(4).innerHTML = "";;
-    row.insertCell(5).innerHTML = "<button id='" + buttonId + "'>Delete</button><br/>";
+    row.insertCell(5).innerHTML = "<button id='" + buttonId + "'class= 'btn btn-danger'>Delete</button><br/>";
 
       bookings.innerHTML += id + "- " + name + ", " + email + ", pax:" + pax + "<br/>"; 
       bookings.innerHTML += "<button id='" + buttonId + "'>Delete</button><br/>";
